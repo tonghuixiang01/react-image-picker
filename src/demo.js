2e77737bc06a9c51334f6c0249288fb9b2021419
Tong Hui Kang <tonghuikang@outlook.com>
2018-10-14 01:16:28 +0800

    combined class

--- src/demo.js
+++ src/demo.js
@@ -88,48 +88,6 @@ function loadGifs(url) {
   });
 }
 
-class Demo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      image: null,
-      images: []
-    }
-  }
-
-  onPickImage(image) {
-    this.setState({ image })
-  }
-
-  onPickImages(images) {
-    // send saved state to firebase
-    console.log("images");
-    this.setState({ images });
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Single Select</h3>
-        <ImagePicker
-          images={imageList.map((image, i) => ({ src: image, value: i }))}
-          onPick={this.onPickImage.bind(this)}
-        />
-        <textarea rows="1" cols="100" value={this.state.image && JSON.stringify(this.state.image)} disabled />
-
-        <h3>Multiple Select</h3>
-        <ImagePicker
-          images={imageList.map((image, i) => ({ src: image, value: i }))}
-          onPick={this.onPickImages.bind(this)}
-          multiple
-        />
-        <textarea rows="8" cols="100" value={this.state.images && JSON.stringify(this.state.images)} disabled />
-      </div>
-    )
-  }
-}
-
-
 
 class Search extends React.Component {
   constructor(props) {
@@ -137,10 +95,23 @@ class Search extends React.Component {
 
     this.state = {
       loading: false,
-      image: ''
+      image: null,
+      images: []
     }
 
     this.search = this.search.bind(this);
+  }
+
+  // when an image (from single select) is selected
+  onPickImage(image) {
+    this.setState({ image })
+  }
+
+  // when an image (from multiple select) is selected
+  onPickImages(images) {
+    // send saved state to firebase
+    console.log("images");
+    this.setState({ images });
   }
 
   // Get initial image
@@ -172,10 +143,30 @@ class Search extends React.Component {
     // Set `search__results` bg image to the image url
     return (
       <div className="search">
-        <form onSubmit={this.search}>
-          <input className="search__input" type="text" placeholder="search..." ref="search" />
-        </form>
-        <div style={divStyle} className="search__results">
+
+        <div>
+          <h3>Single Select</h3>
+          <ImagePicker
+            images={imageList.map((image, i) => ({ src: image, value: i }))}
+            onPick={this.onPickImage.bind(this)}
+          />
+          <textarea rows="1" cols="100" value={this.state.image && JSON.stringify(this.state.image)} disabled />
+
+          <h3>Multiple Select</h3>
+          <ImagePicker
+            images={imageList.map((image, i) => ({ src: image, value: i }))}
+            onPick={this.onPickImages.bind(this)}
+            multiple
+          />
+          <textarea rows="8" cols="100" value={this.state.images && JSON.stringify(this.state.images)} disabled />
+        </div>
+
+        <div>
+          <form onSubmit={this.search}>
+            <input className="search__input" type="text" placeholder="search..." ref="search" />
+          </form>
+          <div style={divStyle} className="search__results">
+          </div>
         </div>
       </div>
     )
@@ -184,4 +175,3 @@ class Search extends React.Component {
 
 var api_key = "RvWFyDT9no5s24ctoMd0CIFRPiEwaVBG"
 render(<Search />, document.getElementById("app"))
-render(<Demo />, document.querySelector('#root'))
